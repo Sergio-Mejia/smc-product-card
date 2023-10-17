7d1a62c51771d026e3914a686cb1aebd31d4373a
Sergio Mejia <sdmejiac@gmail.com>
2023-10-16 19:21:02 -0500

    feat: first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# SMS-Product-Card
+
+Paquete de pruebas de despliegue en npm
+
+### Sergio Mejia
+
+## Ejemplo
+
+```
+import { ProductButtons, ProductCard, ProductImage, ProductTitle } from 'smc-product-card';
+```
+
+```
+<ProductCard
+    product={product}
+    initialValues={{
+        count: 4,
+        maxCount: 10,
+    }}
+    >
+    {() => (
+        <>
+        <ProductImage />
+        <ProductTitle />
+        <ProductButtons />
+        </>
+    )}
+</ProductCard>
+
+```
---
+++ src/components/ProductButtons.tsx
@@ -0,0 +1,46 @@
+import React, { CSSProperties, useCallback, useContext } from "react";
+import { ProductContext } from "./ProductCard";
+import styles from "../styles/styles.module.css";
+
+export interface Props {
+  className?: string;
+  style?: CSSProperties;
+}
+
+export const ProductButtons = ({ className, style }: Props) => {
+  //todo: extraer maxCount
+  const { increaseBy, counter, maxCount } = useContext(ProductContext);
+
+  // const isMaxReached = useCallback(() => {
+  //   if (counter === maxCount) {
+  //     return true;
+  //   } else {
+  //     return false;
+  //   }
+  // }, [counter, maxCount]);
+  
+  const isMaxReached = useCallback(
+    () => !!maxCount && counter === maxCount,
+    [counter, maxCount]
+  );
+
+  //Todo: isMaxReached = useCallback; dependencias [counter, maxCount]
+  //* True si el counter === maxCount
+  //* False si no lo es
+
+  return (
+    <div className={`${styles.buttonsContainer} ${className}`} style={style}>
+      <button className={styles.buttonMinus} onClick={() => increaseBy(-1)}>
+        -
+      </button>
+      <div className={styles.countLabel}>{counter}</div>
+      <button
+        className={`${styles.buttonAdd}  ${ isMaxReached() && styles.disabled}`}
+        onClick={() => increaseBy(+1)}
+        disabled={isMaxReached()}
+      >
+        +
+      </button>
+    </div>
+  );
+};
